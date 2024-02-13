09193f262adb484797e00a9aaf60a8fe146d2745
Muhamad Hardiansyah <mmuhamadhardiansyah@gmail.com>
2024-02-13 17:42:11 +0700

    Type Safe Components

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,41 +1,52 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import Colors from '@/src/constants/Colors';
+import { StyleSheet, Text, View, Image } from "react-native";
+import Colors from "@/src/constants/Colors";
+import { Product } from "../types";
 
-const ProductListItem = ({product}) => {
-  return(
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
     <View style={styles.container}>
-      <Image source={{ uri : product.image }} style={ styles.image } />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
-  )
-}
+  );
+};
 
 export default ProductListItem;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     padding: 10,
-    borderRadius:20,
+    borderRadius: 20,
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
   title: {
     fontSize: 20,
-    fontWeight: '600',
+    fontWeight: "600",
     marginVertical: 10,
   },
   price: {
     color: Colors.light.tint,
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-  image:{
-    width: '100%',
+  image: {
+    width: "100%",
     aspectRatio: 1,
-  }
+  },
 });
